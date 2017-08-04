d39fe9f32f1651ed46d713945a7b8240e125eafc
sergey.sokolov <sergey.sokolov@behavox.com>
2017-08-04 15:39:52 +0300

    Made it runnable.

--- src/index-aot.ts
+++ src/index-aot.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {platformBrowser} from '@angular/platform-browser';
 import {AppModuleNgFactory} from '../compiled/src/app/app.module.ngfactory';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
