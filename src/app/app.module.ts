bdfe57414fadae8919236a30dd63b919d24b4d04
sergey.sokolov <sergey.sokolov@behavox.com>
2017-10-25 16:55:01 +0300

    Added some forms code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import {AppComponent} from './root/app.component';
 import {HomeComponent} from 'home/home.component';
 import {appRoutes, appRoutingOpts} from './app.routing';
 import {EmptyComponent} from './empty/empty.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
               imports: [
                   BrowserModule,
+                  FormsModule,
+                  ReactiveFormsModule,
                   RouterModule.forRoot(appRoutes, appRoutingOpts),
               ],
               declarations: [
--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
                selector: 'app-root',
                templateUrl: './app.component.html',
-               styleUrls: ['./app.component.css']
+               styleUrls: ['./app.component.css'],
            })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'app works!';
+
+    form: FormGroup;
+    name: string;
+
+    constructor(
+        private formBuilder: FormBuilder,
+    ) {
+    }
+
+    ngOnInit() {
+        this.form = this.formBuilder
+            .group({
+                       name: ['', Validators.required],
+                       value: '',
+                   });
+        this.name = this.form.get('name').value;
+        this.name = this.form['name'].value;
+    }
 }
